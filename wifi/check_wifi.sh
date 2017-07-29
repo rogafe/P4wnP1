7a8b0fcf2cf5811b3aa0dd504e3a8efa666fdc3c
rogafe <niamor.rogafe@gmail.com>
2017-07-29 22:35:09 +0200

    modify check_wifi

--- wifi/check_wifi.sh
+++ wifi/check_wifi.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 # check for presence of wlan0
 
-if ! iwconfig 2>&1 | grep -q -E ".*wlan0.*"; then
+if ! sudo iwconfig 2>&1 | grep -q -E ".*wlan0.*"; then
         echo "...[Error] now wlan0 interface found"
         exit 1
 fi
