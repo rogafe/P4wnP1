0bac7bad44df0bf11f0d0f220211e951f0852782
rogafe <niamor.rogafe@gmail.com>
2017-08-22 11:51:38 +0200

    usb0 to usb1

--- chip_hid_activate.sh
+++ chip_hid_activate.sh
@@ -8,15 +8,15 @@ mkdir strings/0x409
 echo "x101x" > strings/0x409/serialnumber
 echo "Skynet" > strings/0x409/manufacturer
 echo "LOL_Human" > strings/0x409/product
-mkdir functions/hid.usb0
-echo 1 > functions/hid.usb0/protocol
-echo 1 > functions/hid.usb0/subclass
-echo 8 > functions/hid.usb0/report_length
-/bin/echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+mkdir functions/hid.usb1
+echo 1 > functions/hid.usb1/protocol
+echo 1 > functions/hid.usb1/subclass
+echo 8 > functions/hid.usb1/report_length
+/bin/echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb1/report_desc
 mkdir configs/c.1
 mkdir configs/c.1/strings/0x409
 echo Conf 1 > configs/c.1/strings/0x409/configuration
 echo 120 > configs/c.1/MaxPower
-ln -s functions/hid.usb0 configs/c.1
+ln -s functions/hid.usb1 configs/c.1
 echo musb-hdrc.1.auto > UDC
 
